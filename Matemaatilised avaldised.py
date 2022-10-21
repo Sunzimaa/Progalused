b931524893fb0b9ffba3efc67b85e9302abce0c2
Marten <marten.kabral@voco.ee>
2022-10-21 11:42:56 +0300

    Ülesanne: Matemaatilised avaldised
    (Poolik)

---
+++ Matemaatilised avaldised.py
@@ -0,0 +1,54 @@
+"""Set of different math expression exercises."""
+
+num_a = 5
+num_b = 2
+
+# 1. Sum and difference
+sum = num_a + num_b
+print(f"{num_a} + {num_b} = {sum}")
+print("--------------------------")
+
+difference = num_a - num_b
+print(f"{num_a} - {num_b} = {difference}")
+print("--------------------------")
+
+# 2. Float division
+division = num_a / num_b
+print(f"{num_a} / {num_b} = {division}")
+print("--------------------------")
+
+# 3. Integer division
+division = num_a // num_b
+print(f"{num_a} // {num_b} = {division}")
+print("--------------------------")
+
+# 4. Powerful operations
+multiply_numbers = num_a * num_b
+print(f"{num_a} * {num_b} = {multiply_numbers}")
+print("--------------------------")
+
+power = num_a ** num_b
+print(f"{num_a} ** {num_b} = {power}")
+print("--------------------------")
+
+remainder = num_a % num_b
+print(f"{num_a} % {num_b} = {remainder}")
+print("--------------------------")
+
+# 5. Find average
+average = (num_a + num_b) / 2
+print(f"({num_a} + {num_b}) / {2} = {average}")
+print("--------------------------")
+
+# 6. Area of a circle
+
+radius = 3.5
+import math
+circle_area = math.pi * radius * radius
+print(f"{math.pi} * {radius} * {radius} = {circle_area}")
+circle_area = (round(circle_area, 2))
+print(circle_area)
+print("--------------------------")
+
+# 7. Area of an equilateral triangle
+side_length =
