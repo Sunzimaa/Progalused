61627e13478f96055f1cd8b2be1aa13af2df4765
Marten <marten.kabral@voco.ee>
2022-10-27 16:28:13 +0300

    Ülesanne: Funktsioon

---
+++ Function.py
@@ -0,0 +1,46 @@
+# 1. Inside the function
+
+def func():
+    print("I´m inside the function")
+
+func()
+
+print("-------------------")
+
+# 2. My name is ...
+
+def my_name_is(name):
+    print(f"my name is {name}")
+
+my_name_is("Mary")
+
+print("-------------------")
+
+# 3. Sum six
+
+def sum_six(num):
+    return (6 + num)
+
+print(sum_six(5))
+
+print("-------------------")
+
+# 4. Sum numbers
+
+def sum_numbers(a, b):
+    return (a + b)
+
+print(sum_numbers(5, 4))
+
+print("-------------------")
+
+#5. USD to EUR
+
+def usd_to_eur(dollar):
+    return round(dollar * 0.8)
+
+print(usd_to_eur(100))
+
+
+
+
