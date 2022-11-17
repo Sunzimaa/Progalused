d3e0febf60ef83846609820542ce5e2775d1ef2b
Marten <marten.kabral@voco.ee>
2022-11-17 14:17:21 +0200

    Ülesanne: Sõne

---
+++ Sõne.py
@@ -0,0 +1,23 @@
+# Write your code here
+
+#1. Sõne-tüüpi muutujate deklareerimine
+
+first_name = "James"
+last_name = "Bond"
+
+#2. Sõnede kujundamine - names
+
+full_name = first_name + " " + last_name
+self_description_sentence = (f"My name is {last_name}, {first_name} {last_name}.")
+
+#3. Sõnede kujundamine - cake string
+
+cake = "vahukoormarjadtäidispõhi"
+print("vahukoor\nmarjad\ntäidis\npõhi")
+
+#4. Sõnede tükeldamine
+
+original_string = "Programming is fun!"
+backwards = original_string[::-1]
+every_other = original_string[::2]
+first_word_reversed = original_string[10::-1]
